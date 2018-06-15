f4a54568da4258b446426bb669c138e65e6996c8
DESKTOP-2RJFIAT\Peter <peterglad1985@hotmail.com>
2018-06-15 23:00:24 +0200

    added various tools

--- toolbox.py
+++ toolbox.py
@@ -1,18 +1,26 @@
-""" The Toolbox module is written by Peter Hecht Glad and contains the following tools to assist with scripting Linux infrastructure: """
+""" The Toolbox module is written by Peter Hecht Glad and contains the following tools: """
+
+import socket
 
 # Command Executor
-def runcmd(command):
+def run_command(command):
     CMD = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
     return CMD.stdout.read()
 
 # Port Check
-def checkport(ip,port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    result = sock.connect_ex((ip,port))
-    return result
+def check_tcp_port(ip, port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex((ip,port))
+        if result == 0:
+            return True
+        else:
+            return False
+    except Exception as e:
+        return False
 
 # IP Syntax Validator
-def is_valid_ipv4_address(address):
+def check_ipv4_syntax(address):
     try:
         socket.inet_pton(socket.AF_INET, address)
     except AttributeError:
@@ -25,26 +33,27 @@ def is_valid_ipv4_address(address):
         return False
     return True
 
-# Commentor and appender
-def comment_and_append(path, start_identifier, text_to_append):
+# Loops through given file and comments lines in and appends a provided string
+def file_comment_and_append(filepath, line_startswith, text_to_append):
     with open(path) as infile:
         with open(path + '.new', 'w') as outfile:
             for line in infile:
-               if line.rstrip().startswith(start_identifier):
+               if line.rstrip().startswith(line_startswith):
                    outfile.write('#' + line + '\n')
                else:
                    outfile.write(line + '\n')
             outfile.write(text_to_append + "\n")
-            runcmd('mv ' + path + ' ' + path + '.old')
-            runcmd('mv ' + path + '.new ' + path)
+            run_command('mv ' + path + ' ' + path + '.old')
+            run_command('mv ' + path + '.new ' + path)
 
-def are_packages_installed(packages):
+# Check if apt package is installed and returns list objects with installed and not installed packages
+def check_apt_packages_status(packages):
     packages = packages.split(' ')
+    packages_installed = []
     packages_not_installed = []
     for package in packages:
-        if '[installed]' in runcmd('apt list ' + package):
-            print('[+] ' + package + ' is installed')
+        if '[installed]' in run_command('apt list ' + package):
+            packages_installed.append(package)
         else:
-            print('[-] ' + package + ' is not installed')
             packages_not_installed.append(package)
-    return packages_not_installed
+    return packages_installed, packages_not_installed
