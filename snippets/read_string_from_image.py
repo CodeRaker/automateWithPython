8ea7a6a14f41357a1388954c06e620bbb83db011
CodeRaker <peterglad1985@hotmail.com>
2019-06-08 21:57:36 +0200

    Update read_string_from_image.py

--- snippets/read_string_from_image.py
+++ snippets/read_string_from_image.py
@@ -1,1 +1,9 @@
+# pip install pytesseract <-- this is only the api to tesseract
 
+from PIL import Image
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe' # tell where tesseract is, must install first
+
+img = Image.open("C:\\Users\\LoL\Desktop\\symantectest.jpg")
+print(pytesseract.image_to_string(img))
